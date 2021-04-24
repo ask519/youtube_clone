2b963d7e3eb715c0c6bb5e18ced5d81b949b633d
Abhishek Kalahal <abhishekkalahal@gmail.com>
2021-04-24 23:02:53 +0530

    Downloads using presigned URLs implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
+require 'aws-sdk-s3'
+
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @videos = Video.where(user_id: params[:id]).where.not(thumbnail_url: nil)
+  end
+
+  def download_video
+    video_key = Video.find(params[:id]).file_name
+    presigner = Aws::S3::Presigner.new
+    video_url = presigner.presigned_url(:get_object, bucket: ENV['AWS_S3_BUCKET_NAME'], key: video_key, expires_in: 18000)
+    redirect_to video_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   get 'auth/failure', :to => 'sessions#failure'
   get '/logout', :to => 'sessions#destroy'
   get '/users/:id', to: 'users#show'
+  post '/users/download/:id', to: 'users#download_video'
 end
