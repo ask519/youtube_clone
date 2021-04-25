85788085e01f8c8573429c4ca2c4214355270142
Abhishek Kalahal <abhishekkalahal@gmail.com>
2021-04-26 00:38:06 +0530

    Added validations to likes and videos

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :user
   enum like_on: [:video, :comment]
+  validates :user_id, :reference, :like_on, :presence => true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,4 @@
 class Video < ApplicationRecord
   belongs_to :user
+  validates :user_id, :file_name, :name, :presence => true
 end
