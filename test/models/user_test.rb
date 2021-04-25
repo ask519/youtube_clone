1b8540dd75a401debcbe62dc238075fbb8583c07
Abhishek Kalahal <abhishekkalahal@gmail.com>
2021-04-26 01:11:29 +0530

    Added user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "shouldn't save a blank user" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "shouldn't save a user without name" do
+    user = User.new
+    user.email = "babymctesterson@gmail.com"
+    user.uid = "yooooooooooo"
+    assert_not user.save
+  end
+
+  test "shouldn't save a user without an email" do
+    user = User.new
+    user.name = "Baby McTesterson"
+    user.uid = "yooooooooooo"
+    assert_not user.save
+  end
+
+  test "shouldn't save a user without an uid" do
+    user = User.new
+    user.name = "Baby McTesterson"
+    user.email = "babymctesterson@gmail.com"
+    assert_not user.save
+  end
+
+  test "should save a user with all attributes" do
+    user = User.new
+    user.name = "Baby McTesterson"
+    user.email = "babymctesterson@gmail.com"
+    user.uid = "yooooooooooo"
+    assert user.save
+  end
 end
